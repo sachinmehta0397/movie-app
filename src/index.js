ecdc64fccb940d8cddb89bf55cd859bbba9a505f
Sachin <sachin.mehta0397@gmail.com>
2021-04-28 22:52:55 +0530

    Added ‘Home’ component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Home from './home/Home';
 
 ReactDOM.render(
-    <div>
-    </div>,
+    <Home/>,
   document.getElementById('root')
 );
 
